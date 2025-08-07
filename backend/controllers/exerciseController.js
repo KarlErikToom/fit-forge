f4128cbbf808a72f68cea9ba379c8422dcd4b937
Karl Erik Toom <toom.karl42@gmail.com>
2025-08-07 13:21:34 +0300

    added update exercise and delete exercise

--- backend/controllers/exerciseController.js
+++ backend/controllers/exerciseController.js
@@ -1,12 +1,10 @@
+const User = require("../models/user.model");
 const UserExercise = require("../models/userExercise.model");
-
-
 
 const createExercise = async (req, res) => {
   try {
     const userId = req.user.id;
-    const exercise = await UserExercise.create({ userId ,...req.body})
-
+    const exercise = await UserExercise.create({ userId, ...req.body });
 
     res.status(200).json(exercise);
   } catch (error) {
@@ -14,14 +12,64 @@ const createExercise = async (req, res) => {
   }
 };
 
-const getExercises = async (req,res)=>{
+const getExercises = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const exercises = await UserExercise.find({ userId });
+    res.status(200).json({ exercises });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getExercise = async (req, res) => {
+  try {
+    const { exerciseId } = req.params;
+    const userId = req.user.id;
+
+    const exercise = await UserExercise.findOne({ _id: exerciseId, userId });
+
+    if (!exercise) {
+      return res.status(404).json({ message: "exercise not found" });
+    }
+
+    res.status(200).json(exercise);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const updateExercise = async (req, res) => {
+  try {
+    const { exerciseId } = req.params;
+    const userId = req.user.id;
+    const updates = req.body;
+
+    const exercise = await UserExercise.findOneAndUpdate(
+      { _id: exerciseId, userId },
+      updates,
+      { new: true }
+    );
+    if (!exercise){
+        return res.status(404).json({message:"exercise not found"})
+    }
+    res.status(200).json({exercise})
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const deleteExercise = async (req,res) =>{
     try {
+        const {exerciseId}= req.params;
         const userId = req.user.id;
-        const exercises = await UserExercise.find({userId})
-        res.status(200).json({exercises})
+
+        const exercise = await UserExercise.findOneAndDelete({_id:exerciseId, userId})
+        if(!exercise){
+            return res.status(404).json({message: "exercise not found"})
+        }
+        res.status(200).json({message:"exercise deleted"})
+
     } catch (error) {
-        res.status(500).json({message:error.message})
+        res.status(500),json({message:error.message})
     }
 }
-
-module.exports = { createExercise, getExercises };
+module.exports = { createExercise, getExercises, getExercise, updateExercise, deleteExercise };
--- backend/routes/exerciseRoutes.js
+++ backend/routes/exerciseRoutes.js
@@ -7,6 +7,9 @@ const { verifyToken } = require("../middleware/auth");
 // API/CLIENT ROUTES
 router.post("/", verifyToken, exerciseController.createExercise)
 router.get("/", verifyToken, exerciseController.getExercises)
+router.get("/:exerciseId", verifyToken, exerciseController.getExercise)
+router.put("/:exerciseId", verifyToken, exerciseController.updateExercise)
+router.delete("/:exerciseId", verifyToken, exerciseController.deleteExercise)
 
 
 // API/CLIENT/WORKOUT ROUTES
