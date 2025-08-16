318da9f830f01184f2c703174ad4b51cb550fbfb
Karl Erik Toom <toom.karl42@gmail.com>
2025-08-16 14:36:45 +0300

    added validation to clients/clientId endpoint

--- frontend/fit-forge/app/clients/[clientId]/page.jsx
+++ frontend/fit-forge/app/clients/[clientId]/page.jsx
@@ -1,10 +1,18 @@
 import { cookies } from "next/headers";
 import ApiClient from "@/lib/api";
 import ClientCalendar from "./ClientCalendar";
+import { redirect } from "next/dist/server/api-utils";
 
 export default async function ClientPage(props) {
   const params = await props.params;
   const cookieStore = await cookies();
+
+  const isLoggedIn = await validateToken(token);
+
+  if (!isLoggedIn){
+    redirect("/login")
+  }
+
   const cookieHeader = cookieStore
     .getAll()
     .map(({ name, value }) => `${name}=${value}`)
@@ -14,4 +22,26 @@ export default async function ClientPage(props) {
   const client = await api.getClient( params.clientId);
 
   return <ClientCalendar client={client} clientId={params.clientId} />;
+}
+
+
+async function validateToken(token) {
+  if (!token) return false;
+
+  try {
+    const res = await fetch("http://localhost:5000/api/auth/check", {
+      headers: {
+        cookie: `token=${token}`,  
+      },
+      cache: "no-store", 
+    });
+
+    if (!res.ok) return false;
+
+    const data = await res.json();
+    return data.loggedIn;
+  } catch (err) {
+    console.error("Auth check failed", err);
+    return false;
+  }
 }
